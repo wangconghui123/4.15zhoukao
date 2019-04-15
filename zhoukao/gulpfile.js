2f2dd368b5f09864ba71c52c5c33693fd74ad884
wch <1114590177@qq.com>
2019-04-15 09:38:48 +0800

    创建webserver服务报错了

--- zhoukao/gulpfile.js
+++ zhoukao/gulpfile.js
@@ -20,3 +20,39 @@ gulp.task("js", function() {
         }))
         .pipe(gulp.dest("./dist/js/"))
 })
+
+//压缩sass
+gulp.task("sass", function() {
+        return gulp.src("./src/sass*.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("./src/sass"))
+    })
+    //压缩图片
+gulp.task("img", function() {
+        return gulp.src("./src/images/*.{png,jpg,jpej}")
+            .pipe(img())
+            .pipe(gulp.dest("./src/images/imgin/"))
+    })
+    //创建服务器
+gulp.task("webserver", function() {
+    return gulp.src('./src/')
+        .pipe(webserver({
+            port: 8080,
+            host: "169.254.19.111",
+            livereload: true,
+            middleware: function(req, res, next) {
+                var pathname = url.path(req.url).path.name;
+                console.log(pathname);
+            }
+        }))
+})
+
+//监听sass文件 编译scss
+gulp.task("watch", function() {
+    gulp.watch("./src/sass*.scss", gulp.series("sass"))
+})
+
+//注册开发任务
+gulp.task("dev", gulp.series("sass", "webserver", "watch"));
+//注册上线任务
+gulp.task("build", gulp.porallel("img", "js"));
---
+++ zhoukao/src/js/all.js
@@ -0,0 +1,9 @@
+function a() {
+    console.log(a.js)
+}
+a();
+
+function b() {
+    console.log(b.js)
+}
+b();
